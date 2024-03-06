fec84ac6677d67894d274caee00c6f4f4cb7bd0d
Siddharth Ganti <gantisid@gmail.com>
2024-03-05 19:45:16 -0500

    Working auth

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,3 +1,5 @@
+import { login, signup } from "@/lib/actions";
+
 import {
     Card,
     CardContent,
@@ -12,6 +14,16 @@ import { Instagram } from "lucide-react";
 export default function Page() {
   return (
     <main className="flex min-h-screen justify-center items-center">
+
+      <form>
+        <label htmlFor="email">Email:</label>
+        <input id="email" name="email" type="email" required />
+        <label htmlFor="password">Password:</label>
+        <input id="password" name="password" type="password" required />
+        <button formAction={login}>Log in</button>
+        <button formAction={signup}>Sign up</button>
+      </form>
+
       <div className="text-center">
         <Card className="h-full w-96">
           <CardHeader>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,32 @@
 import { Button } from "@/components/ui/button";
 
-export default function Home() {
+//
+
+import { redirect } from "next/navigation";
+
+import { createClient } from "@/utils/supabase/server";
+
+import { logout } from "@/lib/actions";
+
+//
+
+export default async function Home() {
+
+  //
+  const supabase = createClient()
+
+  const { data, error } = await supabase.auth.getUser()
+  if (error || !data?.user) {
+    redirect('/login')
+  }
+  //
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <Button>Hello</Button>
+      <Button>Hello {data.user.email}</Button>
+      <form action={logout}>
+        <button>logout</button>
+      </form>
     </main>
   );
 }
