c3aaf7b983fbabacd4bde96975254ee8ddb00cbe
Siddharth Ganti <gantisid@gmail.com>
2024-03-08 16:14:10 -0500

    Update form validation messages

--- src/lib/schema.ts
+++ src/lib/schema.ts
@@ -2,12 +2,12 @@ import { z } from "zod";
 
 export const loginFormSchema = z.object({
   email: z.string().email(),
-  password: z.string().min(1, 'Password is empty').max(64, 'Password cannot be more than 64 characters')
+  password: z.string().min(1, "Password is empty").max(64, "Password cannot be more than 64 characters")
 });
 
 export const signupFormSchema = z.object({
-  fullname: z.string().min(1).max(32),
-  username: z.string().min(1).max(32),
+  fullname: z.string().min(1, "Fullname is empty").max(32, "Fullname cannot be more than 64 characters"),
+  username: z.string().min(1, "Username is empty").max(32, "Username cannot be more than 64 characters"),
   email: z.string().email(),
-  password: z.string().min(1, 'Password is empty').max(64, 'Password cannot be more than 64 characters')
+  password: z.string().min(1, "Password is empty").max(64, "Password cannot be more than 64 characters")
 });
